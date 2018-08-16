5aec8557988d87c21c0b91ce5a9264f07d24e995
Oliver <oliver312@gmail.com>
2018-08-16 14:19:31 -0400

    Updates the plugin to make it work with videojs 7.

--- src/plugin.js
+++ src/plugin.js
@@ -1,58 +1,57 @@
+import videojs from 'video.js';
 import {version as VERSION} from '../package.json';
+
+const Plugin = videojs.getPlugin('plugin');
 
 // Default options for the plugin.
 import defaults from './defaults';
-
 import ShareButton from './ShareButton';
 import ShareOverlay from './ShareOverlay';
 
-// Cross-compatibility for Video.js 5 and 6.
-const registerPlugin = videojs.registerPlugin || videojs.plugin;
-// const dom = videojs.dom || videojs;
-
 /**
- * Function to invoke when the player is ready.
+ * An advanced Video.js plugin. For more information on the API
  *
- * This is a great place for your plugin to initialize itself. When this
- * function is called, the player will have its DOM and child components
- * in place.
- *
- * @function onPlayerReady
- * @param    {Player} player
- *           A Video.js player object.
- *
- * @param    {Object} [options={}]
- *           A plain object containing options for the plugin.
+ * See: https://blog.videojs.com/feature-spotlight-advanced-plugins/
  */
-const onPlayerReady = (player, options) => {
-  player.addClass('vjs-videojs-share');
-  player.getChild('controlBar').addChild('ShareButton', options);
-  player.addChild('ShareOverlay', options);
-};
+class Share extends Plugin {
 
-/**
- * A video.js plugin.
- *
- * In the plugin function, the value of `this` is a video.js `Player`
- * instance. You cannot rely on the player being in a "ready" state here,
- * depending on how the plugin is invoked. This may or may not be important
- * to you; if not, remove the wait for "ready"!
- *
- * @function share
- * @param    {Object} [options={}]
- *           An object of options left to the plugin author to define.
- */
-const share = function(options) {
-  onPlayerReady(this, videojs.mergeOptions(defaults, options));
-};
+  /**
+   * Create a Share plugin instance.
+   *
+   * @param  {Player} player
+   *         A Video.js Player instance.
+   *
+   * @param  {Object} [options]
+   *         An optional options object.
+   *
+   *         While not a core part of the Video.js plugin architecture, a
+   *         second argument of options is a convenient way to accept inputs
+   *         from your plugin's caller.
+   */
+  constructor(player, options) {
+    // the parent class will add player under this.player
+    super(player);
 
-videojs.registerComponent('ShareButton', ShareButton);
-videojs.registerComponent('ShareOverlay', ShareOverlay);
+    this.options = videojs.mergeOptions(defaults, options);
 
-// Register the plugin with video.js.
-registerPlugin('share', share);
+    this.player.ready(() => {
+      this.player.addClass('vjs-share');
+      player.addClass('vjs-videojs-share');
+      player.getChild('controlBar').addChild('ShareButton', options);
+      player.addChild('ShareOverlay', options);
+    });
+  }
+}
+
+// Define default values for the plugin's `state` object here.
+Share.defaultState = {};
 
 // Include the version number.
-share.VERSION = VERSION;
+Share.VERSION = VERSION;
 
-export default share;
+// Register the plugin with video.js.
+videojs.registerComponent('ShareButton', ShareButton);
+videojs.registerComponent('ShareOverlay', ShareOverlay);
+videojs.registerPlugin('share', Share);
+
+export default Share;
